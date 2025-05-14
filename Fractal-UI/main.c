26608f10b46670b0ccd8ba38aee87433382b47e5
lukas <lukasjonkus03@gmail.com>
2025-05-14 17:48:16 +0100

    Created a window using SDL2

--- Fractal-UI/main.c
+++ Fractal-UI/main.c
@@ -2,4 +2,44 @@
 // Created by Lukas Jonkus on 14/05/2025.
 //
 
-#include "main.h"
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("SDL_Init Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // Create an SDL window
+    SDL_Window *win = SDL_CreateWindow("Fractal Creator",
+                                       SDL_WINDOWPOS_CENTERED,
+                                       SDL_WINDOWPOS_CENTERED,
+                                       800, 600,
+                                       SDL_WINDOW_SHOWN);
+    if (!win) {
+        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    // Event loop
+    int running = 1;
+    SDL_Event event;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = 0;
+            }
+        }
+
+        // Here you could clear the screen and draw things
+        // (if you have a renderer or use OpenGL)
+    }
+
+    // Clean up
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+    return 0;
+}
